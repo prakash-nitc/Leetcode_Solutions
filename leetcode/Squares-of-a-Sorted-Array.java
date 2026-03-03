ac52c1e64c401d0830f1cba55493622419ccb6ab
Prakash Kumar Sarangi <prakash_m251250cs@nitc.ac.in>
2026-03-03 22:31:34 +0530

    [977] [Time Beats: 99.87%] [Memory Beats: 75.84%] - LeetPush

--- leetcode/Squares-of-a-Sorted-Array.java
+++ leetcode/Squares-of-a-Sorted-Array.java
@@ -1,25 +1,32 @@
 1class Solution {
 2    public int[] sortedSquares(int[] nums) {
-3       int n= nums.length;
-4       int result[] = new int[n];
-5       int left=0, right=n-1;
+3
+4        int n = nums.length;
+5        int[] result = new int[n];
 6
-7       for (int pos= n-1; pos>=0; pos--)
-8       {
-9        int leftsquare = nums[left]*nums[left];
-10        int rightsquare = nums[right]*nums[right];
-11        if (leftsquare > rightsquare)
-12        {
-13            result[pos]= leftsquare;
-14            left++;
-15        }
-16        else
-17        {
-18            result[pos] = rightsquare;
-19            right--;
-20        }
-21       }
-22       return result;
-23    }
-24}
-25
+7        int left = 0;
+8        int right = n - 1;
+9        int pos = n - 1;   // Fill from the end (largest squares first)
+10
+11        // Continue until pointers cross
+12        while (left <= right) {
+13
+14            int leftSquare = nums[left] * nums[left];
+15            int rightSquare = nums[right] * nums[right];
+16
+17            if (leftSquare > rightSquare) {
+18                result[pos] = leftSquare;
+19                left++; 
+20                pos--;         // Move left pointer forward
+21            } else {
+22                result[pos] = rightSquare;
+23                right--; 
+24                pos--;        // Move right pointer backward
+25            }
+26
+27                       // Move fill position backward
+28        }
+29
+30        return result;
+31    }
+32}
