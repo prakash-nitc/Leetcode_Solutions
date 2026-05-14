542fa3fd2dca2a9a3548ca8798778af667875910
Prakash Kumar Sarangi <prakash_m251250cs@nitc.ac.in>
2026-05-15 03:46:45 +0530

    [200] [Time Beats: 27.89%] [Memory Beats: 92.72%] - LeetPush

--- leetcode/Number-of-Islands.java
+++ leetcode/Number-of-Islands.java
@@ -5,21 +5,28 @@
 5            for (int j = 0; j < grid[0].length; j++) {
 6                if (grid[i][j] == '1') {
 7                    count++;
-8                    dfs(grid, i, j);
+8                    bfs(grid, i, j);
 9                }
 10            }
 11        }
 12        return count;
 13    }
 14    
-15    private void dfs(char[][] grid, int i, int j) {
-16        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
-17            return;
-18        }
-19        grid[i][j] = '0';
-20        dfs(grid, i + 1, j);
-21        dfs(grid, i - 1, j);
-22        dfs(grid, i, j + 1);
-23        dfs(grid, i, j - 1);
-24    }
-25}
+15    private void bfs(char[][] grid, int i, int j) {
+16        Queue<int[]> queue = new LinkedList<>();
+17        queue.offer(new int[]{i, j});
+18        grid[i][j] = '0';
+19        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+20        while (!queue.isEmpty()) {
+21            int[] cell = queue.poll();
+22            for (int[] dir : directions) {
+23                int newI = cell[0] + dir[0];
+24                int newJ = cell[1] + dir[1];
+25                if (newI >= 0 && newI < grid.length && newJ >= 0 && newJ < grid[0].length && grid[newI][newJ] == '1') {
+26                    queue.offer(new int[]{newI, newJ});
+27                    grid[newI][newJ] = '0';
+28                }
+29            }
+30        }
+31    }
+32}
