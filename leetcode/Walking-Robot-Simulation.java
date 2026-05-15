cb3783372fd719bd488bce7f7057f6c454abf0f7
Prakash Kumar Sarangi <prakash_m251250cs@nitc.ac.in>
2026-05-15 21:41:07 +0530

    [874] [Time Beats: 95.18%] [Memory Beats: 79.90%] - LeetPush

--- leetcode/Walking-Robot-Simulation.java
+++ leetcode/Walking-Robot-Simulation.java
@@ -1,36 +1,39 @@
 1class Solution {
 2    public int robotSim(int[] commands, int[][] obstacles) {
-3        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-4        int dir = 0;
-5        int x = 0, y = 0;
-6        int maxDist = 0;
-7        
-8        Set<String> obstacleSet = new HashSet<>();
-9        for (int[] obstacle : obstacles) {
-10            obstacleSet.add(obstacle[0] + "," + obstacle[1]);
-11        }
-12        
-13        for (int command : commands) {
-14            if (command == -2) {
-15                dir = (dir + 3) % 4;
-16            } else if (command == -1) {
-17                dir = (dir + 1) % 4;
-18            } else {
-19                for (int i = 0; i < command; i++) {
-20                    int nx = x + directions[dir][0];
-21                    int ny = y + directions[dir][1];
-22                    
-23                    if (!obstacleSet.contains(nx + "," + ny)) {
-24                        x = nx;
-25                        y = ny;
-26                        maxDist = Math.max(maxDist, x * x + y * y);
-27                    } else {
-28                        break;
-29                    }
-30                }
-31            }
-32        }
-33        
-34        return maxDist;
-35    }
-36}
+3        int[] dx = {0, 1, 0, -1};
+4        int[] dy = {1, 0, -1, 0};
+5        int dir = 0;
+6        int x = 0, y = 0;
+7        int maxDist = 0;
+8        
+9        Set<Long> obstacleSet = new HashSet<>();
+10        for (int[] obstacle : obstacles) {
+11            long hash = ((long)obstacle[0] + 30000) * 60001 + (obstacle[1] + 30000);
+12            obstacleSet.add(hash);
+13        }
+14        
+15        for (int command : commands) {
+16            if (command == -2) {
+17                dir = (dir + 3) % 4;
+18            } else if (command == -1) {
+19                dir = (dir + 1) % 4;
+20            } else {
+21                for (int i = 0; i < command; i++) {
+22                    int nx = x + dx[dir];
+23                    int ny = y + dy[dir];
+24                    long hash = ((long)nx + 30000) * 60001 + (ny + 30000);
+25                    
+26                    if (!obstacleSet.contains(hash)) {
+27                        x = nx;
+28                        y = ny;
+29                        maxDist = Math.max(maxDist, x * x + y * y);
+30                    } else {
+31                        break;
+32                    }
+33                }
+34            }
+35        }
+36        
+37        return maxDist;
+38    }
+39}
