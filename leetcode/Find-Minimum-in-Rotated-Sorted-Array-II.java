f1e2845df3a3ea9afa01269f320eb3655c7a2d22
Prakash Kumar Sarangi <prakash_m251250cs@nitc.ac.in>
2026-05-17 03:11:30 +0530

    [154] [Time Beats: 100.00%] [Memory Beats: 54.09%] - LeetPush

--- leetcode/Find-Minimum-in-Rotated-Sorted-Array-II.java
+++ leetcode/Find-Minimum-in-Rotated-Sorted-Array-II.java
@@ -1,11 +1,11 @@
 1class Solution {
 2    public int findMin(int[] nums) {
-3        int min = nums[0];
-4        for (int i = 1; i < nums.length; i++) {
-5            if (nums[i] < min) {
-6                min = nums[i];
+3        int n = nums.length;
+4        for (int i = 1; i < n; i++) {
+5            if (nums[i] < nums[i - 1]) {
+6                return nums[i];
 7            }
 8        }
-9        return min;
+9        return nums[0];
 10    }
 11}
