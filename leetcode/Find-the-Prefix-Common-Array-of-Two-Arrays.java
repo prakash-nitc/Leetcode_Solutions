747b3e3606a31260de81967f172a3e826e3c0261
Prakash Kumar Sarangi <prakash_m251250cs@nitc.ac.in>
2026-05-21 01:56:09 +0530

    [2657] [Time Beats: 100.00%] [Memory Beats: 85.73%] - LeetPush

--- leetcode/Find-the-Prefix-Common-Array-of-Two-Arrays.java
+++ leetcode/Find-the-Prefix-Common-Array-of-Two-Arrays.java
@@ -1,28 +1,14 @@
-1import java.util.*;
-2
-3class Solution {
-4    public int[] findThePrefixCommonArray(int[] A, int[] B) {
-5        int n = A.length;
-6        int[] ans = new int[n];
-7        Set<Integer> set = new HashSet<>();
-8        int common = 0;
-9
-10        for (int i = 0; i < n; i++) {
-11            if (set.contains(A[i])) {
-12                common++;
-13            } else {
-14                set.add(A[i]);
-15            }
-16
-17            if (set.contains(B[i])) {
-18                common++;
-19            } else {
-20                set.add(B[i]);
-21            }
-22
-23            ans[i] = common;
-24        }
-25
-26        return ans;
-27    }
-28}
+1class Solution {
+2    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+3        int n = A.length;
+4        int[] freq = new int[n + 1];
+5        int[] ans = new int[n];
+6        int common = 0;
+7        for (int i = 0; i < n; i++) {
+8            if (++freq[A[i]] == 2) common++;
+9            if (++freq[B[i]] == 2) common++;
+10            ans[i] = common;
+11        }
+12        return ans;
+13    }
+14}
