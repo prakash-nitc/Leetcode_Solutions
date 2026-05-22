65e1e28b686f4b70e6b5bafe55fec3ce1327392f
Prakash Kumar Sarangi <prakash_m251250cs@nitc.ac.in>
2026-05-22 06:03:44 +0530

    [33] [Time Beats: 1.41%] [Memory Beats: 26.61%] - LeetPush

--- leetcode/Search-in-Rotated-Sorted-Array.java
+++ leetcode/Search-in-Rotated-Sorted-Array.java
@@ -1,11 +1,27 @@
 1class Solution {
 2    public int search(int[] nums, int target) {
-3        for (int i = 0; i < nums.length; i++) {
-4            if (nums[i] == target) {
-5                return i;
-6            }
-7        }
-8        return -1;
-9    }
-10}
-11
+3        int low = 0, high = nums.length - 1;
+4
+5        while (low <= high) {
+6            int mid = low + (high - low) / 2;
+7
+8            if (nums[mid] == target) return mid;
+9
+10            if (nums[low] <= nums[mid]) {
+11                if (target >= nums[low] && target < nums[mid]) {
+12                    high = mid - 1;
+13                } else {
+14                    low = mid + 1;
+15                }
+16            } else {
+17                if (target > nums[mid] && target <= nums[high]) {
+18                    low = mid + 1;
+19                } else {
+20                    high = mid - 1;
+21                }
+22            }
+23        }
+24
+25        return -1;
+26    }
+27}
