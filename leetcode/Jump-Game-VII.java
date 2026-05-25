8e50af8ac1fc9921740efe792413a0c40b12c771
Prakash Kumar Sarangi <prakash_m251250cs@nitc.ac.in>
2026-05-25 21:24:42 +0530

    [1871] [Time Beats: 51.15%] [Memory Beats: 11.55%] - LeetPush

--- leetcode/Jump-Game-VII.java
+++ leetcode/Jump-Game-VII.java
@@ -1,26 +1,30 @@
-1class Solution {
-2    public boolean canReach(String s, int minJump, int maxJump) {
-3        int n = s.length();
-4        if (s.charAt(n - 1) == '1') return false;
-5
-6        boolean[] dp = new boolean[n];
-7        dp[0] = true;
-8        int window = 0;
-9
-10        for (int i = 1; i < n; i++) {
-11            int addIdx = i - minJump;
-12            if (addIdx >= 0 && dp[addIdx]) {
-13                window++;
-14            }
+1import java.util.*;
+2
+3class Solution {
+4    public boolean canReach(String s, int minJump, int maxJump) {
+5        int n = s.length();
+6        if (s.charAt(n - 1) == '1') return false;
+7
+8        Queue<Integer> q = new LinkedList<>();
+9        q.offer(0);
+10        int farthest = 0;
+11
+12        while (!q.isEmpty()) {
+13            int i = q.poll();
+14            if (i == n - 1) return true;
 15
-16            int removeIdx = i - maxJump - 1;
-17            if (removeIdx >= 0 && dp[removeIdx]) {
-18                window--;
-19            }
-20
-21            dp[i] = s.charAt(i) == '0' && window > 0;
-22        }
-23
-24        return dp[n - 1];
-25    }
-26}
+16            int start = Math.max(i + minJump, farthest + 1);
+17            int end = Math.min(i + maxJump, n - 1);
+18
+19            for (int j = start; j <= end; j++) {
+20                if (s.charAt(j) == '0') {
+21                    if (j == n - 1) return true;
+22                    q.offer(j);
+23                }
+24            }
+25            farthest = end;
+26        }
+27
+28        return false;
+29    }
+30}
