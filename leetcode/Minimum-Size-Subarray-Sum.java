7c738af77e2cbbb19545825f6037852d55b3e5d0
Prakash Kumar Sarangi <prakash_m251250cs@nitc.ac.in>
2026-05-30 20:56:01 +0530

    [209] [Time Beats: 99.96%] [Memory Beats: 90.86%] - LeetPush

--- leetcode/Minimum-Size-Subarray-Sum.java
+++ leetcode/Minimum-Size-Subarray-Sum.java
@@ -1,25 +1,24 @@
 1class Solution {
 2    public int minSubArrayLen(int target, int[] nums) {
-3        int low=0,high=0;
-4        int result= Integer.MAX_VALUE;
-5        int sum=0, n= nums.length;
-6
-7        while(high<n)
-8        {
-9            sum= sum+ nums[high];
-10
-11            while(sum>=target)
-12            {
-13                int len = high-low+1;
-14                result= Math.min(result, len);
-15                sum= sum-nums[low];
-16                low++;
-17            }
-18            high++;
-19        }
-20        if( result== Integer.MAX_VALUE)
-21        return 0;
-22        else 
-23        return result;
-24    }
-25}
+3        int high=0, low=0, sum=0,length=0, result=Integer.MAX_VALUE;
+4        int n= nums.length;
+5
+6        while(high < n)
+7        {
+8            sum = sum+ nums[high];
+9            while(sum>=target)
+10            {
+11                
+12                length = high-low+1;
+13                result = Math.min(result, length );
+14                sum = sum- nums[low++];
+15                
+16            }
+17            high++;
+18        }
+19        if ( result == Integer.MAX_VALUE)
+20        return 0;
+21        else
+22        return result;
+23    }
+24}
