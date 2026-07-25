f336b79eb98c5472c328f4b5470f1519f79db3dd
Prakash Kumar Sarangi <prakash_m251250cs@nitc.ac.in>
2026-07-25 12:48:39 +0530

    [33] [Time Beats: 100.00%] [Memory Beats: 65.53%] - LeetPush

--- leetcode/33-Search-in-Rotated-Sorted-Array.java
+++ leetcode/33-Search-in-Rotated-Sorted-Array.java
@@ -1,21 +1,4 @@
 class Solution {
-
-    public int binarySearch(int low, int high,int nums[], int target)
-    {
-        while(low<= high)
-        {
-            int guess= (low+high)/2;
-
-            if(nums[guess]== target)
-                return guess;
-            if(nums[guess]<target)
-                low= guess+1;
-            else
-                high= guess-1;
-        }
-        return -1;
-    }
-
     public int search(int[] nums, int target) {
         int low=0;
         int high= nums.length-1;
@@ -24,22 +7,37 @@ class Solution {
         while(low<=high)
         {
             int guess= (low+high)/2;
+            if(nums[guess]==target)
+                return guess;
+            
             if(nums[guess]> nums[nums.length-1])
             {
-                low= guess+1;
+                
+                if(nums[guess]< target)
+                    low= guess+1;
+                else
+                {
+                    if(nums[0]> target)
+                        low= guess+1;
+                    else
+                        high= guess-1;
+                }
+                continue;
             }
             else
             {
-                index= guess;
-                high= guess-1;
+                if(nums[guess]> target)
+                    high= guess-1;
+                else 
+                {
+                    if(nums[nums.length-1]< target)
+                        high= guess-1;
+                    else
+                        low= guess+1;
+                }
             }
         }
-        int search1= binarySearch(0, index-1,nums, target);
-        int search2= binarySearch(index, nums.length-1,nums, target );
-
-        if(search1== -1)
-            return search2;
-        else
-            return search1;
+        return -1;
+        
     }
 }
