3254ab799eb7b4fa7cd50e2728b6e6dfa25d72c2
Prakash Kumar Sarangi <prakash_m251250cs@nitc.ac.in>
2026-07-26 18:05:22 +0530

    [74] [Time Beats: 100.00%] [Memory Beats: 24.61%] - LeetPush

--- leetcode/74-Search-a-2D-Matrix.java
+++ leetcode/74-Search-a-2D-Matrix.java
@@ -1,40 +1,24 @@
 class Solution {
 
-    public int searchRow(int [][]matrix, int target)
-    {
-        int low=0, high= matrix.length-1, result=-1;;
-        while(low<=high)
-        {
-            int guess= (low+high)/2;
-            if(matrix[guess][0]<= target)
-            {
-                result= guess;
-                low=guess+1;
-            }
-            else
-                high=guess-1;
-        }
-        return result;
-    }
     public boolean searchMatrix(int[][] matrix, int target)
     {
-        int row= searchRow(matrix, target);
-
-        if(row==-1)
-            return false;
-        int low= 0;
-        int high= matrix[0].length-1;
-        int result=-1;
+        int low=0;
+        int no_of_rows= matrix.length, no_of_cols= matrix[0].length;
+        int high= no_of_rows*no_of_cols-1;
 
         while(low<=high)
         {
-            int guess= (low+high)/2;
-            if(matrix[row][guess]==target)
+            int guess=(low+high)/2;
+
+            int row_number= guess/no_of_cols;
+            int col_number= guess%no_of_cols;
+
+            if(matrix[row_number][col_number]==target)
                 return true;
-            else if(matrix[row][guess]>target)
-                high= guess-1; 
-            else
+            else if (matrix[row_number][col_number]<target)
                 low= guess+1;
+            else
+                high=guess-1;
         }
         return false;
         
